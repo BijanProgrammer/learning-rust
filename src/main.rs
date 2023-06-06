4da68dfe5900a3a629876bd9ca25acba445d58c4
BijanProgrammer <bijaneisapour@gmail.com>
2023-06-06 18:27:49 +0330

    [LEARN] communicating with user

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, friend!");
+    let mut name = String::new();
+
+    print!("What's your name? ");
+    io::stdout().flush().expect("failed to flush");
+
+    io::stdin()
+        .read_line(&mut name)
+        .expect("failed to read line");
+
+    println!("Hello, {}!", name.trim());
 }
